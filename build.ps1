029e8a4aea4b3a5ef18e1b44d73ced2b664bd604
xiaoyong <54741819+xiaoyong-wang@users.noreply.github.com>
2024-11-16 17:07:54 -0500

    check if venv

--- build.ps1
+++ build.ps1
@@ -1,2 +1,10 @@
+if (-Not (Test-Path -Path ".venv")) {
+    python -m venv .venv
+    . .\.venv\Scripts\Activate.ps1
+    pip install -r requirements.txt
+} else {
+    . .\.venv\Scripts\Activate.ps1
+}
+
 $pyfigletFontsPath = ".venv/Lib/site-packages/pyfiglet/fonts"
 pyinstaller --onefile --name Ovel --hidden-import=numpy --hidden-import=colorama --hidden-import=pyfiglet --hidden-import=pyfiglet.fonts --hidden-import=colorama.Fore --hidden-import=colorama.Back --hidden-import=colorama.Style --hidden-import=colorama.Init --add-data "$pyfigletFontsPath;pyfiglet/fonts" main.py
