28a03a74c794ae28130f0dbaf3e550f5b28e4d14
xiaoyong <54741819+xiaoyong-wang@users.noreply.github.com>
2024-11-16 17:05:38 -0500

    updates

--- build.ps1
+++ build.ps1
@@ -1,1 +1,2 @@
-pyinstaller --onefile --name Ovel --hidden-import=numpy --hidden-import=colorama --hidden-import=pyfiglet --hidden-import=pyfiglet.fonts --hidden-import=colorama.Fore --hidden-import=colorama.Back --hidden-import=colorama.Style --hidden-import=colorama.Init main.py
+$pyfigletFontsPath = ".venv/Lib/site-packages/pyfiglet/fonts"
+pyinstaller --onefile --name Ovel --hidden-import=numpy --hidden-import=colorama --hidden-import=pyfiglet --hidden-import=pyfiglet.fonts --hidden-import=colorama.Fore --hidden-import=colorama.Back --hidden-import=colorama.Style --hidden-import=colorama.Init --add-data "$pyfigletFontsPath;pyfiglet/fonts" main.py
